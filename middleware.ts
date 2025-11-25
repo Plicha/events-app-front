f71aaa106bd2788025f74dc2194f5f613dabb124
Karol Pliszka <k.pliszka@lemonmind.com>
2025-11-25 21:36:08 +0100

    Update middleware to pass pathname via header

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,19 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './lib/i18n/routing';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+  const response = intlMiddleware(request);
+  
+  if (response) {
+    const pathname = request.nextUrl.pathname;
+    response.headers.set('x-pathname', pathname);
+  }
+  
+  return response;
+}
 
 export const config = {
   matcher: ['/', '/(pl|en)/:path*']
