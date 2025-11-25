0489a48d249f3db328b3d6541d034e4086154ace
Karol Pliszka <k.pliszka@lemonmind.com>
2025-11-25 21:36:11 +0100

    Create MobileMenuButton as Client Component

---
+++ components/layout/Header/MobileMenuButton.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Popover, Button } from 'antd'
+import type { MenuProps } from 'antd'
+import { MenuOutlined } from '@ant-design/icons'
+import { Menu } from 'antd'
+
+interface MobileMenuButtonProps {
+  menuItems: MenuProps['items']
+  selectedKey: string | null
+}
+
+export function MobileMenuButton({ menuItems, selectedKey }: MobileMenuButtonProps) {
+  const mobileMenuContent = (
+    <Menu
+      mode="vertical"
+      selectedKeys={selectedKey ? [selectedKey] : []}
+      items={menuItems}
+      style={{ border: 'none', minWidth: 200 }}
+    />
+  )
+
+  return (
+    <Popover
+      content={mobileMenuContent}
+      trigger="click"
+      placement="bottomRight"
+    >
+      <Button
+        type="text"
+        icon={<MenuOutlined />}
+        className="mobile-menu-button"
+      />
+    </Popover>
+  )
+}
+
